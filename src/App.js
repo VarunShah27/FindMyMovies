6228f469eb8587c9d5d5ac2292827490fb2098d6
Varun Shah <84119192+VarunShah27@users.noreply.github.com>
2025-09-29 13:26:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ const levenshteinDistance = (s1, s2) => {
 function App() {
   const [state, setState] = useState({ s: "", results: [], selected: {} });
   const [suggestion, setSuggestion] = useState("");
-  const apiurl = "http://www.omdbapi.com/?i=tt3896198&apikey=883a4c5";
+  const apiurl = "5d048d70e2ec77e86219f7e5f1256f22";
 
   useEffect(() => {
     const shuffled = popularMovies.sort(() => 0.5 - Math.random());
