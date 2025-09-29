6e800aff3d0171293fa40ae20a292d2f484b1b8a
Varun Shah <84119192+VarunShah27@users.noreply.github.com>
2025-09-29 15:08:31 +0530

    Update Popup.js

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,20 +1,18 @@
-// src/components/Popup.js
-
-import React from 'react';
+import React from "react";
 
 function Popup({ selected, closePopup }) {
   return (
     <section className="popup">
       <div className="content">
         <div className="plot">
-          <img src={selected.Poster} alt={selected.Title} />
+          <img src={selected.poster_path} alt={selected.title} />
           <div className="info">
-            <h2>{selected.Title} <span>({selected.Year})</span></h2>
-            <p className="rating">Rating: ⭐ {selected.imdbRating}</p>
-            <p><strong>Genre:</strong> {selected.Genre}</p>
-            <p><strong>Director:</strong> {selected.Director}</p>
-            <p><strong>Actors:</strong> {selected.Actors}</p>
-            <p>{selected.Plot}</p>
+            <h2>{selected.title} <span>({selected.release_date?.slice(0, 4)})</span></h2>
+            <p className="rating">Rating: ⭐ {selected.vote_average}</p>
+            <p><strong>Genre:</strong> {selected.genres?.map(g => g.name).join(", ")}</p>
+            <p><strong>Director:</strong> {selected.director}</p>
+            <p><strong>Actors:</strong> {selected.cast}</p><br/>
+            <p>{selected.overview}</p>
           </div>
         </div>
         <button className="close" onClick={closePopup}>×</button>
